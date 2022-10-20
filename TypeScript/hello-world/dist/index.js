1f58a378fd8ea62e1c279175539a3e4f7489f872
Prashant Gupta <prashantlfh@gmail.com>
2022-10-20 21:20:27 +0530

    Adding TypeScript Code

--- TypeScript/hello-world/dist/index.js
+++ TypeScript/hello-world/dist/index.js
@@ -11,3 +11,25 @@ var Size;
 ;
 let mySize = Size.Medium;
 console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+let ans = calculateTax(10100, 2021);
+console.log(ans);
+let employee = {
+    id: 1,
+    name: 'Prashant Gupta',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function KgtoLbs(weight) {
+    if (typeof weight == 'number')
+        return weight * 2.2;
+    return parseInt(weight) * 2.2;
+}
+let ans1 = KgtoLbs(10);
+let ans2 = KgtoLbs('20');
+console.log(ans1, ans2);
--- TypeScript/hello-world/src/index.ts
+++ TypeScript/hello-world/src/index.ts
@@ -6,3 +6,41 @@ console.log(user)
 enum Size {Small=1,Medium,large};
 let mySize :Size =Size.Medium;
 console.log(mySize)
+
+function calculateTax(income:number,taxYear=2022):number{
+    if(taxYear<2022)
+        return income*1.2;
+    return income*1.3;
+}
+
+let ans:number=calculateTax(10_100,2021)
+console.log(ans);
+
+type Employee={
+    readonly id:number,
+    name:string,
+    retire:(date:Date)=>void
+}
+let employee:Employee ={
+    id:1,
+    name:'Prashant Gupta',
+    retire:(date:Date)=>{
+        console.log(date);
+    }
+};
+
+// Union Type
+
+function KgtoLbs(weight:number | string):number{
+    if(typeof weight=='number')
+        return weight*2.2;
+    return parseInt(weight)*2.2;
+}
+
+let ans1=KgtoLbs(10);
+let ans2=KgtoLbs('20');
+console.log(ans1,ans2);
+
+// Literals Type
+type Quantity=50|100;
+let quantity:Quantity=50;
